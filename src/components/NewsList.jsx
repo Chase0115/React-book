89a1fc63933be326d2b76c0506f49c8b3e849600
Chase <devchase0115@gmail.com>
2022-08-18 23:41:54 +1000

    News Category with Promise Hook

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from '../../node_modules/axios/index';
 import NewsItem from './NewsItem';
+import usePromise from '../lib/usePromis';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,34 +18,26 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({category}) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    }
-    fetchData();
-  }, [category]);
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+    )
+  }, [category])
 
   if (loading) {
     return <NewsListBlock>Loading ...</NewsListBlock>
   }
 
-  if(!articles) {
+  if(!response) {
     return null;
   }
 
+  if(error) {
+    return <NewsListBlock>Error!!</NewsListBlock>
+  }
+
+  const {articles} = response.data;
 
   return (
     <NewsListBlock>
