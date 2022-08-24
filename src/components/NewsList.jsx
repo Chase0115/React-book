03e304ffbc7399364c865b7c1598cdc0f428d773
chase0115 <devchase0115@gmail.com>
2022-08-24 20:02:21 +1000

    no message

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import axios from '../../node_modules/axios/index';
 import NewsItem from './NewsItem';
